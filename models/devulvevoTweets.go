85551339ac6dbae32e64c3661f8b5a4ce1a45d61
Juan <slim.shady99q@gmail.com>
2022-01-30 15:45:01 -0500

    Quick fix

--- models/devulvevoTweets.go
+++ models/devulvevoTweets.go
@@ -8,7 +8,7 @@ import (
 
 type DevulevoTweets struct {
 	ID      primitive.ObjectID `bson:"_id," json:"_id,omitempty"`
-	UserID  string             `bson:"userId" json:"userId,omitempty"`
+	UserID  string             `bson:"userid" json:"userId,omitempty"`
 	Mensaje string             `bson:"mensaje" json:"mensaje,omitempty"`
 	Fecha   time.Time          `bson:"fecha" json:"fecha,omitempty"`
 }
